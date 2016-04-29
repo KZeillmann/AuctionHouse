f006f04a1ef11ec755c35e66972c7523a2ce184b
KZeillmann <kzeillmann@gmail.com>
2016-04-28 23:52:53 -0500

    Added items with scaffolding, added some info on auctionResponse and an auction example file

---
+++ app/models/item.rb
@@ -0,0 +1,24 @@
+class Item < ActiveRecord::Base
+  #auc id
+  #item id
+  #owner
+  #ownerRealm
+  #bid
+  #buyout
+  #quantity
+  #timeLeft
+  #rand
+  #seed
+  #context
+
+  #for an actual item:
+  #item id
+  #description
+  #name
+  #ItemsController#stackable
+  #itemBind
+  #bonusStats
+  #buyPrice
+  #sellPrice
+  #displayInfoId
+end
